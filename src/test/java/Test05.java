770c102d4a70ad40da97b4838c820dee190a4323
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-07-17 08:59:36 +0900

    Stringの拡張例のコードを追加

---
+++ src/test/java/Test05.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Stringの拡張")
+public class Test05 {
+
+  @Test
+  @DisplayName("たとえば、Srtring Format")
+  void case01() {
+    String greeting = "Hello, %s %s !".formatted("Higuma", "Duke");
+
+    assertThat(greeting).isEqualTo("Hello, Higuma Duke !");
+//    fail();
+  }
+
+  @Test
+  @DisplayName("たとえば、Srtring transform")
+  void case02() {
+    String message = "Hello, Higuma Duke !";
+    String digest = message.transform(s -> digest(s));
+
+    assertThat(digest).isEqualTo("Hello...");
+//    fail();
+  }
+
+  private String digest(String str) {
+    if (str.length() > 5) {
+      return str.substring(0, 5) + "...";
+    }
+    return str;
+  }
+
+
+}
