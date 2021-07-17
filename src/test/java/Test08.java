531924018139349404c1671524fe368a003b1b12
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2021-07-17 10:04:22 +0900

    Stream APIの拡張例を追加

---
+++ src/test/java/Test08.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Streamの拡張")
+public class Test08 {
+
+  @Test
+  @DisplayName("Streamの結果をimmutableListで取得する")
+  void case01() {
+    List<Integer> origList = List.of(2, 3, 4, 5);
+    List<Double> powList = origList.stream()
+      .map(i -> Math.pow(i, 2))
+      .toList();
+
+    assertThat(powList).containsExactly(4d, 9d, 16d, 25d);
+  }
+
+
+}
